81084f70c0a0e3682c3cc84ead31cff6829b8dec
gokul chapparath <gokulchapparath@gmail.com>
2019-10-18 00:36:22 +0530

    oct 18 00:36

--- server.py
+++ server.py
@@ -1,7 +1,20 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, static_url_path='/static')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(25), unique=True, nullable=False) 
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    contact = db.Column(db.String(13), unique=True, nullable=False)
+    types = db.Column(db.String(15), nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.contact}',)"
 
 @app.route("/masteru")
 def user():
