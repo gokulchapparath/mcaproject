419994e905815df3f211ea741a678865775703df
gokul chapparath <gokulchapparath@gmail.com>
2019-11-10 21:05:21 +0530

    pdf

--- pdftoimg/convert.py
+++ pdftoimg/convert.py
@@ -1,4 +1,7 @@
 from pdf2image import convert_from_path
+from datetime import datetime
+now = datetime.now().strftime("%d%m%Y%H%M%S%ms")
 pages = convert_from_path('tests.pdf', 500)
+name = now + '.png'
 for page in pages:
-    page.save('out.jpg', 'JPEG')
+    page.save(name, 'PNG')
