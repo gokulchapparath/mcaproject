2c21f40e03c03d2f515f5d647bf1f38f1667b088
gokulchapparath <gokulchapparath@gmail.com>
2019-11-14 14:01:40 +0530

    multiple pdf

---
+++ pdftoimg/mulconvert.py
@@ -0,0 +1,13 @@
+from pdf2image import convert_from_path
+from datetime import datetime
+import os
+
+now = datetime.now().strftime("%d%m%Y%H%M%S%ms")
+filename = 'sample.pdf'
+pages = convert_from_path(filename, 500)
+name = now + '.png'
+dirs = '/home/aru/Desktop/pdftoimg/test/'
+base_filename  =  os.path.splitext(os.path.basename(name))[0] + '.png'
+print(enumerate(pages))
+for i, page in enumerate(pages):
+    page.save(os.path.join(dirs, name + str(i) ), 'PNG')
